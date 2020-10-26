be0596e9b33f1c7e3587b8253249cb93d657f5b0
TimmmyD <tmd112593@gmail.com>
2020-10-26 12:21:47 -0700

    commit tree 6e373d2ff1ff0d823c2fd8504a64ebfbd5b11226

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+    
+    word_array = sentence.split(" ") 
+    word_array.collect do |word|
+       word.reverse!
+    end
+    word_array.join(" ")
+end
